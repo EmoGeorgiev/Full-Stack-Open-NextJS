3d33ec0681ee72b6c786fd4cec0605340289201d
Emo Georgiev <emogeorgiev788@gmail.com>
2026-06-02 17:30:43 +0300

    Added a solution to exercise 11

--- blogs-app/app/services/blogs.ts
+++ blogs-app/app/services/blogs.ts
@@ -1,6 +1,7 @@
-import { eq, like, sql } from "drizzle-orm"
+import { eq, like } from "drizzle-orm"
 import { db } from "@/db"
 import { blogs } from "@/db/schema"
+import { getCurrentUser } from "./session"
 
 export const getBlogs = async (filter?: string) => {
   if (filter) {
@@ -16,13 +17,13 @@ export const getBlogs = async (filter?: string) => {
 }
 
 export const addBlog = async (title: string, author: string, url: string) => {
-  const user = await db.query.users.findFirst({
-    orderBy: sql`RANDOM()`,
-  })
+  const user = await getCurrentUser()
 
-  if (user) {
-    await db.insert(blogs).values({ title, author, url, userId: user.id })
+  if (!user) {
+    throw new Error("Not logged in")
   }
+
+  await db.insert(blogs).values({ title, author, url, userId: user.id })
 }
 
 export const getBlogById = async (id: number) => {
