ed71194b3b5a004d24050924b13b7bbc55b2b94b
Emo Georgiev <emogeorgiev788@gmail.com>
2026-06-06 16:45:33 +0300

    Made a fix to .github

---
+++ blogs-app/test-results/.playwright-artifacts-1/traces/resources/c7fb5cdb3861e7406a05c28513ed6360171e3197.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/app_globals_0jn8.0u.css","static/chunks/node_modules_01po7pd._.js","static/chunks/app_components_0e8-dzm._.js"],
+    source: "dynamic"
+});
